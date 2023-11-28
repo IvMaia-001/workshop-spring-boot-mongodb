9f0a6cfee0c5985344e718c90e8818fea562d118
Ivan Maia <ivanleonardo_maia@hotmail.com>
2023-11-28 15:45:31 -0300

    Obtendo um usuário por id

--- src/main/java/com/ivanmaiainfo/workshopingmongo/services/UserService.java
+++ src/main/java/com/ivanmaiainfo/workshopingmongo/services/UserService.java
@@ -1,22 +1,28 @@
 package com.ivanmaiainfo.workshopingmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ivanmaiainfo.workshopingmongo.domain.User;
 import com.ivanmaiainfo.workshopingmongo.repository.UserRepository;
+import com.ivanmaiainfo.workshopingmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+			return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 
 }
