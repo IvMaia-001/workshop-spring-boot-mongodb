1f58e631a4c8fd212d6d7bd660f8a74de3d5dc48
Ivan Maia <ivanleonardo_maia@hotmail.com>
2023-11-30 12:09:18 -0300

    Consulta com vários critérios

--- src/main/java/com/ivanmaiainfo/workshopingmongo/resources/PostResource.java
+++ src/main/java/com/ivanmaiainfo/workshopingmongo/resources/PostResource.java
@@ -1,38 +1,43 @@
 package com.ivanmaiainfo.workshopingmongo.resources;
-
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import com.ivanmaiainfo.workshopingmongo.domain.Post;
 import com.ivanmaiainfo.workshopingmongo.resources.util.URL;
 import com.ivanmaiainfo.workshopingmongo.services.PostService;
 
 @RestController
-@RequestMapping(value = "/posts")
+@RequestMapping(value="/posts")
 public class PostResource {
-	
+
 	@Autowired
-	private PostService service;
-	
-	@GetMapping (value = "/{id}")
-	public ResponseEntity<Post> findById(@PathVariable String id){
+	private PostService service; 
+
+	@GetMapping("/{id}")
+ 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-
-	@GetMapping(value = "/titlesearch")
-	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+	
+	@GetMapping("/titlesearch")
+ 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
 		return ResponseEntity.ok().body(list);
 	}
-	
-	
+
+	@GetMapping("/fullsearch")
+ 	public ResponseEntity<List<Post>> fullSearch(
+ 			@RequestParam(value="text", defaultValue="") String text,
+ 			@RequestParam(value="minDate", defaultValue="") String minDate,
+ 			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = (Date) URL.convertDate(minDate, new Date(0L));
+		Date max = (Date) URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+	}
 }
--- src/main/java/com/ivanmaiainfo/workshopingmongo/resources/util/URL.java
+++ src/main/java/com/ivanmaiainfo/workshopingmongo/resources/util/URL.java
@@ -2,6 +2,10 @@ package com.ivanmaiainfo.workshopingmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 
@@ -11,6 +15,17 @@ public class URL {
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			return "";
+		}
+	}
+	
+	public static Object convertDate(String textDate, Date defaultValue) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			return sdf.parseObject(textDate);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			return defaultValue;
 		}
 	}
 	
